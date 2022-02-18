571b831ab98ff7f7ea973895e4de014a9067ffd1
BADGumby <badgumby@users.noreply.github.com>
2022-02-18 13:43:50 -0500

    Create credentials.h template

---
+++ include/credentials.h
@@ -0,0 +1,12 @@
+// https://xkcd.com/936/
+
+// WiFi settings
+const char* ssid = "My Home Wifi";
+const char* password = "correcthorsebatterystaple";
+
+// Home Assistant Credentials
+const char *HA_USER = "my_ha_user";
+const char *HA_PASS = "Tr0ub4dor&3";
+
+// IP address of your external MQTT broker, or Home Assistant MQTT Broker
+IPAddress broker(192,168,1,100);
