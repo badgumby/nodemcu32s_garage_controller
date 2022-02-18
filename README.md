5d145ba0e8076b7d1b7e86ef922d07ec9b123d20
BAD Gumby <badgumby@users.noreply.github.com>
2022-02-18 13:40:40 -0500

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# nodemcu32s_garage_controller
-NodeMCU-32s Garage Controller
+# NodeMCU-32s Garage Controller
+
+This project is still a WIP, so the code is yet to be optimized. The goal is to turn my two regular garage doors into "smart" garage doors. This controller will be the base platform using a NodeMCU-32s (or similarly cloned SoC).
+  
+## Sensors/Devices Attached
+ 
+There are multiple things connected to this control board, with more to be added in the future. 
+  
+- Two magnetic reed switches
+  - Similar item on Amazon: [Gikfun MC-38 Wired Door Sensor Magnetic Switch](https://www.amazon.com/dp/B0154PTDFI?psc=1&ref=ppx_yo2_dt_b_product_details)
+
+- Two 3mm green LEDs pre-soldered 460 ohm resistors
+  - Similar item, but white, on Amazon: [KeeYees 60Pcs 3mm Pre Wired LED](https://www.amazon.com/KeeYees-Diodes-Plastic-Mounting-Holders/dp/B07M9YT8C8/ref=sr_1_3?crid=1WI4HWRRCNXY3&keywords=3mm+5v+led&qid=1645209466&s=industrial&sprefix=3mm+5v+led%2Cindustrial%2C95&sr=1-3)
+
+- One 2-channel 5VDC relay module
+  - Similar item on Amazon: [SainSmart 2-Channel Relay Module](https://www.amazon.com/dp/B0057OC6D8?psc=1&ref=ppx_yo2_dt_b_product_details)
+
+## Additional Materials
+
+These are other things that I will be using in this project.
+
+- Speaker wire
+  - Run from the relay to the garage door button sockets.
+
+- Printed circuit board
+  - Just a generic PCB large enough to accommodate solder points for LEDs, relay leads, etc.
+
+- 3D printer filament
+  - I will be 3D printing an enclosure for this and will add it to the project once complete.
+
---
+++ src/main.cpp
@@ -0,0 +1,216 @@
+#include <Arduino.h>      // Used for Arduino framework (which ESP32 chips support)
+#include <WiFi.h>         // Used to connect to WiFi
+#include <PubSubClient.h> // Used for MQTT communication
+#include "credentials.h"  // Header file containing wifi and HA creds
+
+// Sensor pins
+const int DOOR_1_PIN = 16; // Magnetic reed switch
+const int DOOR_2_PIN = 17; // Magnetic reed switch
+const int LED_1_PIN = 21; // LED used for Door 1
+const int LED_2_PIN = 22; // LED used for Door 2
+const int RELAY_1_PIN = 2; // Relay to toggle Door 1
+const int RELAY_2_PIN = 4; // Relay to toggle Door 2
+
+// Default states (0 = Closed, 1 = Open, 2 = Fetch new state)
+int DOOR_1_STATE = 0;
+int DOOR_2_STATE = 0;
+int LAST_DOOR_1_STATE = 2;
+int LAST_DOOR_2_STATE = 2;
+int LOOP_COUNTER = 0; // This is used to send checkin every 15 seconds
+
+// MQTT settings/topics
+const char *ID = "gumby_garage_controller";  // Name of our device, must be unique
+const char *REFRESH_LISTENER = "gumby_garage/refresh_status"; // Used to trigger a forced update
+const char *DOOR_1_POS_TOPIC = "gumby_garage/door_1_pos"; // Position of Door 1
+const char *DOOR_2_POS_TOPIC = "gumby_garage/door_2_pos"; // Position of Door 1
+const char *DOOR_1_RELAY_TOPIC = "gumby_garage/door_1_toggle"; // Toggle door open/close
+const char *DOOR_2_RELAY_TOPIC = "gumby_garage/door_2_toggle"; // Toggle door open/close
+
+// Setup WiFi client
+WiFiClient wclient;
+// Setup MQTT client
+PubSubClient client(wclient);
+bool state=0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  // Fetch current door states
+  DOOR_1_STATE = digitalRead(DOOR_1_PIN);
+  DOOR_2_STATE = digitalRead(DOOR_2_PIN);
+
+  // Print topic and message to serial
+  Serial.print("Topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  // Convert message from char* to string
+  String message;
+  for (int i = 0; i < length; i++) {
+    message = message + (char)payload[i];
+  }
+  Serial.println(message);
+
+  // Decision tree for received topic messages
+  if (strcmp(topic, "gumby_garage/door_1_toggle")==0) {
+    // If message is to close, and the door is currently open
+    if(message == "close" && DOOR_1_STATE == 1) {
+      digitalWrite(RELAY_1_PIN,HIGH); // Turn on relay for Door 1
+      delay(500); // Wait .5 seconds
+      digitalWrite(RELAY_1_PIN,LOW); // Turn off relay for Door 1
+      client.publish(DOOR_1_POS_TOPIC, "0"); // Notify MQTT broker door should be closed
+      delay(2000); // Wait 2 seconds
+      LAST_DOOR_1_STATE = 2; // Reset door state so void Loop can fetch new state
+    }
+    // If message is to open, and the door is currently closed
+    if(message == "open" && DOOR_1_STATE != 1) {
+      digitalWrite(RELAY_1_PIN,HIGH); // Turn on relay for Door 1
+      delay(500); // Wait .5 seconds
+      digitalWrite(RELAY_1_PIN,LOW); // Turn off relay for Door 1
+      client.publish(DOOR_1_POS_TOPIC, "100"); // Notify MQTT broker door should be open
+      delay(2000); // Wait 2 seconds
+      LAST_DOOR_1_STATE = 2; // Reset door state so void Loop can fetch new state
+    } 
+  } else if (strcmp(topic, "gumby_garage/door_2_toggle")==0) {
+    // If message is to close, and the door is currently open
+    if(message == "close" && DOOR_2_STATE == 1) {
+      digitalWrite(RELAY_2_PIN,HIGH); // Turn on relay for Door 2
+      delay(500);
+      digitalWrite(RELAY_2_PIN,LOW); // Turn on relay for Door 2
+      client.publish(DOOR_2_POS_TOPIC, "0"); // Notify MQTT broker door should be closed
+      delay(2000); // Wait 2 seconds
+      LAST_DOOR_2_STATE = 2; // Reset door state so void Loop can fetch new state
+    }
+    // If message is to open, and the door is currently closed
+    if(message == "open" && DOOR_2_STATE != 1) {
+      digitalWrite(RELAY_2_PIN,HIGH); // Turn on relay for Door 2
+      delay(500); // Wait .5 seconds
+      digitalWrite(RELAY_2_PIN,LOW); // Turn off relay for Door 2
+      client.publish(DOOR_2_POS_TOPIC, "100"); // Notify MQTT broker door should be open
+      delay(2000); // Wait 2 seconds
+      LAST_DOOR_2_STATE = 2; // Reset door state so void Loop can fetch new state
+    }
+  } else if (strcmp(topic, "gumby_garage/refresh_status")==0) {
+    Serial.print("Sending door state refresh...");
+    LAST_DOOR_1_STATE = 2; // Reset door state so void Loop can fetch new state
+    LAST_DOOR_2_STATE = 2; // Reset door state so void Loop can fetch new state
+  } else {
+    // Listening to topic, but can't something went wrong
+    Serial.print("Error on topic: '");
+    Serial.print(topic);
+    Serial.print("' with message: ");
+    Serial.println(message);
+  }
+ 
+  Serial.println("-----------------------");
+ 
+}
+
+// Reconnect to client
+void reconnect() {
+  // Loop until reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(ID,HA_USER,HA_PASS)) {
+      Serial.println("connected");
+      Serial.println("Publishing to the following topics: ");
+      Serial.println(DOOR_1_POS_TOPIC);
+      Serial.println(DOOR_2_POS_TOPIC);
+
+    } else {
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+  // MQTT subscription topics
+  client.subscribe(REFRESH_LISTENER);
+  client.subscribe(DOOR_1_RELAY_TOPIC);
+  client.subscribe(DOOR_2_RELAY_TOPIC);
+  Serial.println("-----------------------");
+}
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  // Start serial monitoring
+  Serial.begin(115200);
+  // Connect to wifi
+  initWiFi();
+  // Print out the RSSI (in dB)
+  Serial.print("RSSI: ");
+  Serial.println(WiFi.RSSI());
+  // Set pin modes
+  pinMode(DOOR_1_PIN, INPUT_PULLUP);
+  pinMode(DOOR_2_PIN, INPUT_PULLUP);
+  pinMode(LED_1_PIN, OUTPUT);
+  pinMode(LED_2_PIN, OUTPUT);
+  pinMode(RELAY_1_PIN, OUTPUT);
+  pinMode(RELAY_2_PIN, OUTPUT);
+  // Start MQTT publisher
+  client.setServer(broker, 1883);
+  // Start MQTT subscriber
+  client.setCallback(callback);
+}
+
+void loop() {
+  // Reconnect if connection is lost
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Increment loop counter
+  LOOP_COUNTER++;
+
+  // Read current door states
+  DOOR_1_STATE = digitalRead(DOOR_1_PIN);
+  DOOR_2_STATE = digitalRead(DOOR_2_PIN);
+
+  // Monitor magnet switch for Door 1
+  if (DOOR_1_STATE != LAST_DOOR_1_STATE) {
+    if (DOOR_1_STATE == 1) {
+      digitalWrite(LED_1_PIN, HIGH); // LED On
+      Serial.println("Door 1: open"); // Print status to serial monitor
+      client.publish(DOOR_1_POS_TOPIC, "100"); // MQTT Door 1 open
+    } else {
+      digitalWrite(LED_1_PIN, LOW); // LED Off
+      Serial.println("Door 1: closed"); // Print status to serial monitor
+      client.publish(DOOR_1_POS_TOPIC, "0"); // MQTT Door 1 closed
+    }
+  }
+   
+  // Monitor magnet switch for Door 2
+  if (DOOR_2_STATE != LAST_DOOR_2_STATE) {
+    if (DOOR_2_STATE == 1) {
+      digitalWrite(LED_2_PIN, HIGH);  // LED On
+      Serial.println("Door 2: open"); // Print status to serial monitor
+      client.publish(DOOR_2_POS_TOPIC, "100"); // MQTT Door 2 open
+    } else {
+      digitalWrite(LED_2_PIN, LOW); // LED Off
+      Serial.println("Door 2: closed"); // Print status to serial monitor
+      client.publish(DOOR_2_POS_TOPIC, "0"); // MQTT Door 2 closed
+    }
+  }
+
+  // Check loop counter for MQTT checkin interval
+  // This is useful for sending updates in case Home Assistant has restarted
+  // Sometimes Home Assistant will put a device in an unknown state on HA startup, and will not update it's state until it's received a new message
+  if (LOOP_COUNTER >= 30) {
+    LAST_DOOR_1_STATE = 2; // Force door state fetch on next loop
+    LAST_DOOR_2_STATE = 2; // Force door state fetch on next loop
+    LOOP_COUNTER = 0; // Reset loop counter
+  } else {
+    LAST_DOOR_1_STATE = DOOR_1_STATE; // Update previous door state to current door state
+    LAST_DOOR_2_STATE = DOOR_2_STATE; // Update previous door state to current door state
+  }
+  
+  delay(500); // Wait .5 seconds before restarting the loop
+}
